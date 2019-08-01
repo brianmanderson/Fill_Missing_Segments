7a5c7b4b9e12cc685ffb60dc32844089756042da
Brian Mark Anderson <bmanderson@mdanderson.org>
2019-08-01 13:47:45 -0500

    Main function

---
+++ Fill_In_Segments_sitk.py
@@ -0,0 +1,82 @@
+import SimpleITK as sitk
+import numpy as np
+import os, sys
+if os.path.exists('\\\\mymdafiles\\di_data1\\'):
+    sys.path.append(os.path.join('..', '..', '..'))
+    from Keras_Utils import plot_scroll_Image
+
+
+def get_bounding_box_indexes(annotation):
+    '''
+    :param annotation: A binary image of shape [# images, # rows, # cols, channels]
+    :return: the min and max z, row, and column numbers bounding the image
+    '''
+    annotation = np.squeeze(annotation)
+    if annotation.dtype != 'int':
+        annotation[annotation>0.1] = 1
+        annotation = annotation.astype('int')
+    indexes = np.where(np.any(annotation, axis=(1, 2)) == True)[0]
+    min_z_s, max_z_s = indexes[0], indexes[-1]
+    '''
+    Get the row values of primary and secondary
+    '''
+    indexes = np.where(np.any(annotation, axis=(0, 2)) == True)[0]
+    min_r_s, max_r_s = indexes[0], indexes[-1]
+    '''
+    Get the col values of primary and secondary
+    '''
+    indexes = np.where(np.any(annotation, axis=(0, 1)) == True)[0]
+    min_c_s, max_c_s = indexes[0], indexes[-1]
+    return min_z_s, int(max_z_s + 1), min_r_s, int(max_r_s + 1), min_c_s, int(max_c_s + 1)
+
+class Fill_Missing_Segments(object):
+    def __init__(self):
+        MauererDistanceMap = sitk.SignedMaurerDistanceMapImageFilter()
+        MauererDistanceMap.SetInsideIsPositive(True)
+        MauererDistanceMap.UseImageSpacingOn()
+        MauererDistanceMap.SquaredDistanceOff()
+        self.MauererDistanceMap = MauererDistanceMap
+    def make_distance_map(self, pred, liver, reduce=True):
+        '''
+        :param pred: A mask of your predictions with N channels on the end, N=0 is background [# Images, 512, 512, N]
+        :param liver: A mask of the desired region [# Images, 512, 512]
+        :param MauererDistanceMap: Filter
+        :param reduce:
+        :return:
+        '''
+        spacing = (0.975,0.975, 2.5)
+        pred = pred.astype('int')
+        min_z, min_r, max_r, min_c, max_c = 0, 0, 512, 0, 512
+        max_z = pred.shape[0]
+        if reduce:
+            min_z, max_z, min_r, max_r, min_c, max_c = get_bounding_box_indexes(liver)
+        reduced_pred = pred[min_z:max_z,min_r:max_r,min_c:max_c]
+        reduced_liver = liver[min_z:max_z,min_r:max_r,min_c:max_c]
+        reduced_output = np.zeros(reduced_pred.shape)
+        for i in range(1,pred.shape[-1]):
+            temp_reduce = reduced_pred[...,i]
+            image = sitk.GetImageFromArray(temp_reduce)
+            image.SetSpacing(spacing)
+            output = self.MauererDistanceMap.Execute(image)
+            reduced_output[...,i] = sitk.GetArrayFromImage(output)
+        reduced_output[reduced_output>0] = 0
+        reduced_output = np.abs(reduced_output)
+        reduced_output[...,0] = np.inf
+        output = np.zeros(reduced_output.shape,dtype='int')
+        mask = reduced_liver == 1
+        values = reduced_output[mask]
+        output[mask,np.argmin(values,axis=-1)] = 1
+        pred[min_z:max_z,min_r:max_r,min_c:max_c] = output
+        return pred
+
+# Fill_Segments = Fill_Missing_Segments()
+#
+# liver = np.load(os.path.join('.','liver.npy'))
+# pred = np.load(os.path.join('.','pred.npy'))
+# output = Fill_Segments.make_distance_map(pred,liver)
+# xxx = 1
+
+if __name__ == '__main__':
+    xxx = 1
+    # Fill_Segments = Fill_Missing_Segments()
+    # output = Fill_Segments.make_distance_map(pred,liver)
