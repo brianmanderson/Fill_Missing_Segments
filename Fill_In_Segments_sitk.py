0d148cb73525b21c834e39bdbf50f60248af6710
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2019-11-26 14:25:49 -0600

    added remove_non_liver

--- Fill_In_Segments_sitk.py
+++ Fill_In_Segments_sitk.py
@@ -1,5 +1,7 @@
 import SimpleITK as sitk
 import numpy as np
+import copy
+from skimage import morphology
 
 
 def get_bounding_box_indexes(annotation):
@@ -24,6 +26,50 @@ def get_bounding_box_indexes(annotation):
     indexes = np.where(np.any(annotation, axis=(0, 1)) == True)[0]
     min_c_s, max_c_s = indexes[0], indexes[-1]
     return min_z_s, int(max_z_s + 1), min_r_s, int(max_r_s + 1), min_c_s, int(max_c_s + 1)
+
+
+def remove_non_liver(annotations, threshold=0.5, volume_threshold=9999999, do_3D = True, do_2D=False):
+    annotations = copy.deepcopy(annotations)
+    annotations = np.squeeze(annotations)
+    if not annotations.dtype == 'int':
+        annotations[annotations < threshold] = 0
+        annotations[annotations > 0] = 1
+        annotations = annotations.astype('int')
+    if do_3D:
+        labels = morphology.label(annotations, neighbors=4)
+        if np.max(labels) > 1:
+            area = []
+            max_val = 0
+            for i in range(1,labels.max()+1):
+                new_area = labels[labels == i].shape[0]
+                if new_area > volume_threshold:
+                    continue
+                area.append(new_area)
+                if new_area == max(area):
+                    max_val = i
+            labels[labels != max_val] = 0
+            labels[labels > 0] = 1
+            annotations = labels
+    if do_2D:
+        slice_indexes = np.where(np.sum(annotations,axis=(1,2))>0)
+        if slice_indexes:
+            for slice_index in slice_indexes[0]:
+                labels = morphology.label(annotations[slice_index], connectivity=1)
+                if np.max(labels) == 1:
+                    continue
+                area = []
+                max_val = 0
+                for i in range(1, labels.max() + 1):
+                    new_area = labels[labels == i].shape[0]
+                    if new_area > volume_threshold:
+                        continue
+                    area.append(new_area)
+                    if new_area == max(area):
+                        max_val = i
+                labels[labels != max_val] = 0
+                labels[labels > 0] = 1
+                annotations[slice_index] = labels
+    return annotations
 
 
 class Fill_Missing_Segments(object):
